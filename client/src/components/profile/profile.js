a2c4850e5d009bdd571aa02e45e4a317c8bd7dea
rupponi <upponirohan@gmail.com>
2019-12-02 04:13:22 -0700

    Update:
     - Began profile view with follower count.

--- client/src/components/profile/profile.js
+++ client/src/components/profile/profile.js
@@ -1,29 +1,64 @@
 import React, { Component } from 'react';
 import './profile.css';
 
+const BACKEND_URI = 'http://localhost:7000';
+
+async function getUser() {
+    return fetch(`${BACKEND_URI}/user`, {
+                method: 'GET',
+                headers: {
+                    'Accept' : 'application/json',
+                    'Content-Type' : 'application/json'
+                }
+            })
+            .then(response => response.json());
+}
+
 class Profile extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            message: ''
+            profilePic: 'img/generic-profile-icon.jpg',
+            name: '',
+            following: 0,
+            followers: 0
         };
     }
 
     componentDidMount() {
-        fetch('/login')
-        .then((response) => response.json())
-        .then((responseJson) => {
-            alert(responseJson);
-            this.setState({
-                message: responseJson.data
+        let loadState = {
+            profilePic: 'img/generic-profile-icon.jpg',
+            name: '',
+            following: 0,
+            followers: 0
+        };
+
+        getUser()
+            .then(user => {
+                let userData = JSON.parse(user);
+
+                loadState.profilePic = userData.images[0].url;
+                loadState.name = userData.display_name;
+                loadState.followers = userData.followers.total;
+            })
+            .then(() => {
+                this.setState(loadState);
             });
-        })
     }
 
     render() {
         return(
             <div id = "profile-component">
+                <img id = "profile-picture" class = "unselectable" src = {this.state.profilePic}/>
+                <h1 id = "profile-name" class = "unselectable">{this.state.name}</h1>
+
+                <div id = "follow-section">
+                    <div id = "followers-section">
+                        <h1 class = "follow-text unselectable">FOLLOWERS</h1>
+                        <h1 class = "follow-num unselectable">{this.state.followers}</h1>
+                    </div>
+                </div>
             </div>
         );
     }
